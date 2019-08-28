5161b41c5d3fc1b1b644969e4360e24452a859e0
lljxyh <jvlunl@163.com>
2019-08-28 14:47:45 +0800

    剑指offer update 2018-08-28

--- 剑指offer.py
+++ 剑指offer.py
@@ -148,17 +148,195 @@
 
 """
 # -*- coding:utf-8 -*-
+# class Solution:
+#     def Permutation(self, ss):
+#         # write code here
+#         if not ss:
+#             return []
+#         res = []
+#         self.digui(ss, res, '')
+#         return sorted(list(set(res)))
+#
+#     def digui(self, ss, res, path):
+#         if not ss:
+#             res.append(path)
+#         else:
+#             for i in range(len(ss)):
+#                 self.digui(ss[:i]+ss[i+1:], res, path+ss[i])
+#
+#
+#
+# so = Solution()
+# so.Permutation('abc')
+
+
+"""
+数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+例如输入一个长度为9的数组{1,2,3,2,2,2,5,4,2}。由于数字2在数组中出现了5次，
+超过数组长度的一半，因此输出2。如果不存在则输出0。
+"""
+
+# -*- coding:utf-8 -*-
+# class Solution:
+#     def MoreThanHalfNum_Solution(self, numbers):
+#         # write code here
+#         '''
+#         进行排序
+#         :param numbers:
+#         :return:
+#         '''
+#         numbers.sort()
+#         count = len(numbers)
+#         des = numbers[count // 2]
+#         cou = numbers.count(des)
+#         if cou > count//2:
+#
+#             return numbers[count // 2]
+#         else:
+#             return 0
+#
+# so = Solution()
+# print(so.MoreThanHalfNum_Solution([1, 2, 3, 2, 2, 2, 5, 4, 2]))
+
+"""
+输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
+"""
+# -*- coding:utf-8 -*-
+# class Solution:
+#     def GetLeastNumbers_Solution(self, tinput, k):
+#         # write code here
+#         if k>len(tinput):
+#             return []
+#         tinput.sort()
+#         return tinput[0:k]
+
+
+"""
+HZ偶尔会拿些专业问题来忽悠那些非计算机专业的同学。
+今天测试组开完会后,
+他又发话了:在古老的一维模式识别中,常常需要计算连续子向量的最大和,
+当向量全为正数的时候,问题很好解决。
+但是,如果向量中包含负数,是否应该包含某个负数,并期望旁边的正数会弥补它呢？
+例如:{6,-3,-2,7,-15,1,2,2},连续子向量的最大和为8(从第0个开始,到第3个为止)。
+给一个数组，返回它的最大连续子序列的和，你会不会被他忽悠住？(子向量的长度至少是1)
+"""
+
+# -*- coding:utf-8 -*-
+# class Solution:
+#     def FindGreatestSumOfSubArray(self, array):
+#         # write code here
+#         a_count = len(array)
+#         a_dict = {}
+#         b_dict = {}
+#         i = 0
+#         while i< a_count:
+#             if array[i] > 0:
+#                 # 求正串的和
+#                 sum1, id1 = self.get_sum(array, i, 1)
+#                 a_dict[id1-1] = sum1
+#                 i = id1
+#                 continue
+#             if array[i] < 0:
+#                 sum1, id1 = self.get_sum(array, i, 0)
+#                 a_dict[id1-1] = sum1
+#                 i = id1
+#                 continue
+#             if array[i] == 0:
+#                 i = i+1
+#                 continue
+#
+#
+#     def get_sum(self, array, i, flag):
+#         if flag == 1:
+#             # 求正串的和
+#             res = []
+#             for j in range(i, len(array)):
+#                 if array[j] < 0:
+#                     break
+#                 res.append(array[j])
+#             return sum(res), j
+#         elif flag == 0:
+#             # 求负串的和
+#             res = []
+#             for j in range(i, len(array)):
+#                 if array[j] > 0:
+#                     break
+#                 res.append(array[j])
+#             return sum(res), j
+
+# class Solution:
+#     def FindGreatestSumOfSubArray(self, array):
+#         maxsum, maxhere = array[0], array[0]
+#         for i in array[1:]:
+#             if maxhere <= 0:
+#                 maxhere = i
+#             else:
+#                 maxhere += i
+#             if maxhere > maxsum:
+#                 maxsum = maxhere
+#         return maxsum
+#
+#
+# so = Solution()
+# so.FindGreatestSumOfSubArray([6, -3, -2, 7, -15, 1, 2, 2])
+
+
+"""
+求出1~13的整数中1出现的次数,并算出100~1300的整数中1出现的次数？
+为此他特别数了一下1~13中包含1的数字有1、10、11、12、13因此共出现6次,但是对于后面问题他就没辙了。
+ACMer希望你们帮帮他,并把问题更加普遍化,可以很快的求出任意非负整数区间中1出现的次数（从1 到 n 中1出现的次数）。
+"""
+
+
+# -*- coding:utf-8 -*-
+# class Solution:
+#     def NumberOf1Between1AndN_Solution(self, n):
+#         # write code here
+#         a_str = ''
+#         for i in range(1, n + 1):
+#             a_str += str(i)
+#         return a_str.count('1')
+
+"""
+输入一个正整数数组，把数组里所有数字拼接起来排成一个数，
+打印能拼接出的所有数字中最小的一个。例如输入数组{3，32，321}，则打印出这三个数字能排成的最小数字为321323。
+"""
+
+# -*- coding:utf-8 -*-
+# class Solution:
+#     def PrintMinNumber(self, numbers):
+#         if not numbers:
+#             return ''
+#         # write code here
+#         numbers = [str(i) for i in numbers]
+#         numbers = self.Permutation(numbers)
+#         numbers = [int(i) for i in numbers]
+#         return min(numbers)
+#
+#     def Permutation(self, ss):
+#         # write code here
+#         if not ss:
+#             return []
+#         res = []
+#         self.digui(ss, res, '')
+#         return sorted(list(set(res)))
+#
+#     def digui(self, ss, res, path):
+#         if not ss:
+#             res.append(path)
+#         else:
+#             for i in range(len(ss)):
+#                 self.digui(ss[:i]+ss[i+1:], res, path+ss[i])
+
+"""
+把只包含质因子2、3和5的数称作丑数（Ugly Number）。
+例如6、8都是丑数，但14不是，
+因为它包含质因子7。 习惯上我们把1当做是第一个丑数。求按从小到大的顺序的第N个丑数。
+"""
+# -*- coding:utf-8 -*-
 class Solution:
-    def Permutation(self, ss):
+    def GetUglyNumber_Solution(self, index):
         # write code here
-        if not ss:
-            return
-
-
-
-
-
-
 
 
 
